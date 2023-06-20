3ed70149e60736f3ec29197d02f2667bdc05b948
Daniel chuks <danieluchechukwuchuks1@gmail.com>
2023-06-20 11:33:12 +0100

    Transaction history design implemented

--- src/app/reusableComponents/transaction-modal/transaction-modal.component.ts
+++ src/app/reusableComponents/transaction-modal/transaction-modal.component.ts
@@ -2,7 +2,10 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { OverlayEventDetail } from '@ionic/core/components';
 import { Router } from '@angular/router';
 import { IonModal, ModalController } from '@ionic/angular';
-import { PDFGenerator, PDFGeneratorOptions } from '@ionic-native/pdf-generator/ngx';
+import {
+  PDFGenerator,
+  PDFGeneratorOptions,
+} from '@ionic-native/pdf-generator/ngx';
 
 @Component({
   selector: 'app-transaction-modal',
@@ -10,21 +13,23 @@ import { PDFGenerator, PDFGeneratorOptions } from '@ionic-native/pdf-generator/n
   styleUrls: ['./transaction-modal.component.scss'],
 })
 export class TransactionModalComponent implements OnInit {
-
   @ViewChild(IonModal) modal: IonModal;
   name: string;
   message = '';
   getSelectedHistory;
   content: string;
 
-
-  constructor( private route: Router,
+  constructor(
+    private route: Router,
     private modalCtrl: ModalController,
-    private pdfGenerator: PDFGenerator,) { }
+    private pdfGenerator: PDFGenerator
+  ) {}
 
   ngOnInit() {
-   this.getSelectedHistory =  JSON.parse(sessionStorage.getItem('selectedHistory'));
-   //console.log(this.getSelectedHistory);
+    this.getSelectedHistory = JSON.parse(
+      sessionStorage.getItem('selectedHistory')
+    );
+    //console.log(this.getSelectedHistory);
   }
 
   onWillDismiss(event: Event) {
@@ -45,15 +50,15 @@ export class TransactionModalComponent implements OnInit {
       documentSize: 'A4',
       type: 'share',
       // landscape: 'portrait',
-      fileName: 'Transaction.pdf'
+      fileName: 'Transaction.pdf',
     };
-    this.pdfGenerator.fromData(this.content, options)
+    this.pdfGenerator
+      .fromData(this.content, options)
       .then((base64) => {
         //console.log('OK', base64);
-      }).catch((error) => {
+      })
+      .catch((error) => {
         //console.log('error', error);
       });
-
   }
-
 }
