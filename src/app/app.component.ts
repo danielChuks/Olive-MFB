d88be41f81842491026bd5af5f35dda8d6aee9c4
Cindy Eke <offcialcee1@gmail.com>
2023-06-26 16:36:59 +0100

    replaced ion-slides with swiper js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { IdleTimerService } from './idle-timer.service';
 import { AlertController } from '@ionic/angular';
-
+import { register } from 'swiper/element/bundle';
+register();
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
