a0a9d43c4b234c76cc06ed8dced98e4bed4c6266
Daniel chuks <danieluchechukwuchuks1@gmail.com>
2023-08-03 10:25:54 +0100

    fixed issues with transaction history

--- src/app/hometab/transfers-modal/transfers-modal.component.ts
+++ src/app/hometab/transfers-modal/transfers-modal.component.ts
@@ -7,26 +7,22 @@ import { Route, Router } from '@angular/router';
   styleUrls: ['./transfers-modal.component.scss'],
 })
 export class TransfersModalComponent implements OnInit {
+  constructor(private modalCtrl: ModalController, private router: Router) {}
 
-  constructor(private modalCtrl: ModalController,  private router: Router,) { }
+  ngOnInit() {}
 
-  ngOnInit() { }
-
-  closeModal(type){
+  closeModal(type) {
     if (type === 'same') {
       this.router.navigateByUrl('/new-tab/hometab/transfer');
       this.modalCtrl.dismiss();
       return;
-    }
-    else if (type === 'other') {
-       this.router.navigateByUrl('/other-transfer');
+    } else if (type === 'other') {
+      this.router.navigateByUrl('/other-transfer');
       this.modalCtrl.dismiss();
-       return;
-    }
-    else {
+      return;
+    } else {
       this.modalCtrl.dismiss(null, type);
-       return;
+      return;
     }
   }
-
 }
